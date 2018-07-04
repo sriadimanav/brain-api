f1de75d3d796b9ca9ec1df57071b08523c3cb4ea
sriadimanav <30376679+sriadimanav@users.noreply.github.com>
2018-07-05 03:45:40 +0530

    heroku database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-octagonal-24440',
     user : 'postgres',
     password : 'swineflu',
     database : 'brain'
