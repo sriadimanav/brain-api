50e23a7f02a9642d0b494ea00e1132584915e876
sriadimanav <30376679+sriadimanav@users.noreply.github.com>
2018-07-05 03:52:17 +0530

    heroku database change

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-octagonal-24440',
-    user : 'postgres',
-    password : 'swineflu',
-    database : 'brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
